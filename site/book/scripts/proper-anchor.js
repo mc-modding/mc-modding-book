ce902d8a0b9dbae9389b0fc7dd2a90858728421c
CMTV <cmtvpetya@gmail.com>
2017-04-28 18:56:33 +0300

    Возможность вызывать меню жестом (как в ВК)

--- site/book/scripts/proper-anchor.js
+++ site/book/scripts/proper-anchor.js
@@ -1,23 +1,22 @@
 /**
  * Proper anchor linking
  */
-{
-    const TOP_MARGIN = 20;
 
-    function adjustAnchor() {
-        const $anchor = $(':target');
-        const fixedElementHeight = $('header').outerHeight();
-        if ($anchor.length > 0)
-            window.scrollTo(0, $anchor.offset().top - fixedElementHeight - TOP_MARGIN);
-    }
-    $(window).on('hashchange load', adjustAnchor);
+const TOP_MARGIN = 20;
 
-    $(() => {
-        $('body').on('click', "a[href^='#']", function (ev) {
-            if (window.location.hash === $(this).attr('href')) {
-                ev.preventDefault();
-                adjustAnchor();
-            }
-        });
-    });
+function adjustAnchor() {
+    const $anchor = $(':target');
+    const fixedElementHeight = $('header').outerHeight();
+    if ($anchor.length > 0)
+        window.scrollTo(0, $anchor.offset().top - fixedElementHeight - TOP_MARGIN);
 }
+$(window).on('hashchange load', adjustAnchor);
+
+$(() => {
+    $('body').on('click', "a[href^='#']", function (ev) {
+        if (window.location.hash === $(this).attr('href')) {
+            ev.preventDefault();
+            adjustAnchor();
+        }
+    });
+});
--- site/book/scripts/site-menu-call-controller.js
+++ site/book/scripts/site-menu-call-controller.js
@@ -1,63 +1,62 @@
 /**
  * Site menu call-button controller
  */
-{
-    /**
-     * Is site menu showing right now?
-     *
-     * @type {boolean}
-     */
-    let is_site_menu_showing = false;
 
-    /**
-     * DOM element of button that calls site menu
-     *
-     * @type {Object}
-     */
-    let site_menu_caller;
+/**
+ * Is site menu showing right now?
+ *
+ * @type {boolean}
+ */
+let is_site_menu_showing = false;
 
-    /**
-     * DOM element that contains all menu items
-     *
-     * @type {Object}
-     */
-    let vertical_site_menu;
+/**
+ * DOM element of button that calls site menu
+ *
+ * @type {Object}
+ */
+let site_menu_caller;
 
-    $(() => {
-        init_site_menu_caller();
+/**
+ * DOM element that contains all menu items
+ *
+ * @type {Object}
+ */
+let vertical_site_menu;
 
-        site_menu_caller.click(() => {
-            toggle_vertical_site_menu();
-        });
+$(() => {
+    init_site_menu_caller();
 
-        $(window).click((e) => {
-            if (!$(e.target).closest('.menu-item').length && !$(e.target).closest('.show-menu').length) {
-                close_vertical_site_menu();
-            }
-        });
+    site_menu_caller.click(() => {
+        toggle_vertical_site_menu();
     });
 
-    /**
-     * Init variables connected to site menu caller
-     */
-    function init_site_menu_caller() {
-        site_menu_caller = $('.show-menu');
-        vertical_site_menu = $('.site-menu.transformable');
-    }
+    $(window).click((e) => {
+        if (!$(e.target).closest('.menu-item').length && !$(e.target).closest('.show-menu').length) {
+            close_vertical_site_menu();
+        }
+    });
+});
 
-    function open_vertical_site_menu() {
-        vertical_site_menu.addClass('shown');
-        site_menu_caller.addClass('active');
-        is_site_menu_showing = true;
-    }
+/**
+ * Init variables connected to site menu caller
+ */
+function init_site_menu_caller() {
+    site_menu_caller = $('.show-menu');
+    vertical_site_menu = $('.site-menu.transformable');
+}
 
-    function close_vertical_site_menu() {
-        vertical_site_menu.removeClass('shown');
-        site_menu_caller.removeClass('active');
-        is_site_menu_showing = false;
-    }
+function open_vertical_site_menu() {
+    vertical_site_menu.addClass('shown');
+    site_menu_caller.addClass('active');
+    is_site_menu_showing = true;
+}
 
-    function toggle_vertical_site_menu() {
-        (is_site_menu_showing ? close_vertical_site_menu() : open_vertical_site_menu());
-    }
+function close_vertical_site_menu() {
+    vertical_site_menu.removeClass('shown');
+    site_menu_caller.removeClass('active');
+    is_site_menu_showing = false;
+}
+
+function toggle_vertical_site_menu() {
+    (is_site_menu_showing ? close_vertical_site_menu() : open_vertical_site_menu());
 }
