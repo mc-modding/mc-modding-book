926b5efaca7fd664122c946fa16f692266922b5a
CMTV <cmtvpetya@gmail.com>
2017-05-01 14:15:11 +0300

    Более удобное управление жестами

--- site/book/scripts/swipe-nav-controller.js
+++ site/book/scripts/swipe-nav-controller.js
@@ -1,54 +1,131 @@
-let nav;
+let nav, article;
 
-let start_tolerance = 10;
+let tolerance = 10;
 
-let can_open = false;
-
-let swipe_body = {
-    swipeStatus: (event, phase, direction, distance, duration, fingerCount, fingerData, currentDirection) => {
-        if(Layout.is_layout(LAYOUT_WIDTH.TABLET)) {
-            console.log(event);
-            //if(Utils.between(event.touches[0].clientX, 0, start_tolerance)) {
-                can_open = true;
-            //}
-        }
-    },
-    swipeRight: (event, direction, distance, duration, fingerCount, fingerData) => {
-        if(can_open) {
-            $('nav').removeClass().addClass('shown');
-            call_nav_button.addClass('active');
-            $('article').addClass('darker');
-        }
-    },
-    threshold: 25
-};
-
-let swipe_nav = {
-    swipeLeft: (event, direction, distance, duration, fingerCount, fingerData) => {
-        can_open = false;
-        $('nav').removeClass();
-        $('article').removeClass('darker');
-        call_nav_button.removeClass('active');
-    },
-    threshold: 25
-};
+let nav_width;
 
 $(window).resize(() => {
-    activate_swipe_if();
+    nav_width = nav.outerWidth();
 });
 
 $(() => {
-    activate_swipe_if();
+    nav = $('nav');
+    article = $('article');
+
+    nav_width = nav.outerWidth();
+
+    let hammer_article = new Hammer(article.get(0));
+    let hammer_nav = new Hammer(nav.get(0));
+
+    /* -------------------------------------------------------------------------------------------------------------- */
+    /* Обработка свайпов */
+    /* -------------------------------------------------------------------------------------------------------------- */
+
+    /* Открытие навигации свайпом */
+    hammer_article.on('swiperight', e => {
+        let start_X = e.changedPointers[0].clientX - e.deltaX;
+
+        if(Utils.between(start_X, 0, tolerance)) {
+            open_nav();
+        }
+    });
+
+    /* Закрытие навигации свайпом */
+    hammer_nav.on('swipeleft', () => {
+        close_nav();
+    });
+
+    /* -------------------------------------------------------------------------------------------------------------- */
+    /* Обработка перемещений */
+    /* -------------------------------------------------------------------------------------------------------------- */
+
+    /* Открытие навигации перемещением */
+    hammer_article.on('panleft panright', e => {
+        if(nav.hasClass('shown')) {
+            return;
+        }
+
+        let start_X = e.changedPointers[0].clientX - e.deltaX;
+
+        if(Utils.between(start_X, 0, tolerance)) {
+            nav.addClass('moving');
+        }
+
+        if(nav.hasClass('moving') && nav_right() <= nav_width) {
+            nav_right((e.changedPointers[0].clientX > nav_width ? nav_width : e.changedPointers[0].clientX));
+        }
+    });
+
+    /* Закрытие навигации перемещением */
+    hammer_nav.on('panleft panright', e => {
+        if(!nav.hasClass('shown')) {
+            return;
+        }
+
+        nav.addClass('moving');
+
+        if(nav_right() <= nav_width) {
+            nav_right((nav_width + e.deltaX > nav_width ? nav_width : nav_width + e.deltaX));
+        }
+    });
+
+    /* Отмена перемещения */
+    hammer_article.on('panend pancancel', () => {
+        nav.removeClass('moving');
+
+        if(nav.hasClass('shown')) {
+            return;
+        }
+
+        if(nav_right() > nav_width/2) {
+            open_nav();
+        } else {
+            close_nav();
+        }
+    });
+    hammer_nav.on('panend pancancel', () => {
+        nav.removeClass('moving');
+
+        if(!nav.hasClass('shown')) {
+            return;
+        }
+
+        if(nav_right() < nav_width/2) {
+            close_nav();
+        } else {
+            open_nav();
+        }
+    });
 });
 
-function activate_swipe_if() {
-    if(Layout.is_layout(LAYOUT_WIDTH.TABLET)) {
-        console.log('SWIPE ACTIVATED!');
-        $('body').swipe(swipe_body);
-        $('nav').swipe(swipe_nav);
+function open_nav() {
+    clean_nav();
+    nav.addClass('shown');
+}
+
+function close_nav() {
+    clean_nav();
+}
+
+/** Полная очистка навигации от любых стилей */
+function clean_nav() {
+    nav.removeClass().css('left', '');
+}
+
+/**
+ * Получение позиции правого края навигации. Если передан аргумент, то устанавливает
+ * правый край в заданную точку относительно левой границы окна.
+ *
+ * @param {number|null} value Положение правой границы навигации относительно левой границы окна
+ *
+ * @return {number|null}
+ */
+function nav_right(value = null) {
+    if(value === null) {
+        let nav_pos = nav.position();
+
+        return nav_pos.left + nav_width;
     } else {
-        console.log('SWIPE DESTROYED!');
-        $('body').swipe('destroy');
-        $('nav').swipe('destroy');
+        nav.css('left', `${value - nav_width}px`);
     }
 }
